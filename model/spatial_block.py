e7370f36f90c5974260d52eca904b433c9f26380
pantianyi527 <98876418+pantianyi527@users.noreply.github.com>
2025-04-02 14:15:14 +0800

    Add files via upload

---
+++ model/spatial_block.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from math import sqrt
+
+class Dif_cov(nn.Module):
+    def __init__(self, adj, device):
+        super(Dif_cov, self).__init__()
+        self.device = device
+        self.adj_one = self.normalization(adj)
+        
+    def normalization(self, adjacency):
+        identity = torch.diag_embed(torch.ones_like(adjacency.diag()))
+        adjacency = adjacency + identity
+        degree = torch.diag(adjacency.sum(dim=1))
+        d_diffusion = 1 / degree
+        d_diffusion[[d_diffusion == float('inf')]] = 0.
+        L = d_diffusion @ adjacency
+        return L
+
+    def forward(self, x):
+        x = torch.einsum('ncvl,vw->ncwl', (x, self.adj_one))
+        return x
+
+class Graph_Generator(nn.Module):
+    def __init__(self, c_in, c_out, drop):
+        super(Graph_Generator, self).__init__()
+        self.drop = drop
+        self.w = nn.Conv2d(c_in, c_out, 1)
+        self.norm = 1 / sqrt(c_in)
+        self.avg = nn.AvgPool2d(kernel_size=(c_out, 1), stride=1)
+
+    def forward(self, x):
+        l = self.w(x)
+        l = F.softmax(l, dim=3)
+        x1 = torch.einsum('ncvl,nqvl->ncqv', x, l)
+        q_n = self.avg(x1).squeeze(2)
+        attention = torch.matmul(q_n.transpose(1, 2), q_n) * self.norm
+        adp = F.softmax(F.relu(attention), dim=2)
+        return adp
+
+class spatial_block(nn.Module):
+    def __init__(self, drop, hid_dim, emb_dim, adj, device):
+        super(spatial_block, self).__init__()
+        self.drop = drop
+        self.dif_cov = Dif_cov(adj, device)
+        self.spatial_graph = Graph_Generator(hid_dim, emb_dim, drop)
+        
+    def forward(self, x):
+        x = self.dif_cov(x)
+        adp = self.spatial_graph(x)
+        out = torch.einsum('ncvl,nvw->ncwl', (x, adp))
+        out = F.dropout(out, self.drop, training=self.training)
+        return out
---
+++ model/temporal_block.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Tcn(nn.Module):
+    def __init__(self, c_in, c_out):
+        super(Tcn, self).__init__()
+        self.filter_convs = nn.ModuleList()
+        self.gate_convs = nn.ModuleList()
+        self.filter_convs=torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 2), padding=(0, 0), stride=(1, 1), bias=True)
+        self.gate_convs=torch.nn.Conv1d(c_in, c_out, kernel_size=(1, 2), padding=(0, 0), stride=(1, 1), bias=True)
+
+    def forward(self,x):
+        x = nn.functional.pad(x, (1, 0, 0, 0))
+        filter = self.filter_convs(x)
+        filter = torch.tanh(filter)
+        gate = self.gate_convs(x)
+        gate = torch.sigmoid(gate)
+        x = filter * gate
+        return x
+
+class temporal_graph_generator(nn.Module):
+    def __init__(self, num_nodes, num_steps, device, N_t, temporal_dim):
+        super(temporal_graph_generator, self).__init__()
+        self.nodetime = nn.Parameter(torch.randn(N_t, temporal_dim[0]).to(device),
+                                    requires_grad=True).to(device)
+        self.nodenum = nn.Parameter(torch.randn(num_nodes, temporal_dim[1]).to(device),
+                                     requires_grad=True).to(device)
+        self.timevec1 = nn.Parameter(torch.randn(num_steps, temporal_dim[2]).to(device),
+                                     requires_grad=True).to(device)
+        self.timevec2 = nn.Parameter(torch.randn(num_steps, temporal_dim[3]).to(device),
+                                     requires_grad=True).to(device)
+        self.k = nn.Parameter(torch.randn(temporal_dim[0], temporal_dim[1], temporal_dim[2], temporal_dim[3]).to(device),
+                              requires_grad=True).to(device)
+    def forward(self, t):
+        nodetime = self.nodetime[t]
+        nodenum = self.nodenum
+        timevec1 = self.timevec1
+        timevec2 = self.timevec2
+        k = self.k
+        adp1 = torch.einsum("ad, defg->aefg", [nodetime, k])
+        adp2 = torch.einsum("he, aefg->ahfg", [nodenum, adp1])
+        adp3 = torch.einsum("bf, ahfg->ahbg", [timevec1, adp2])
+        adp4 = torch.einsum("cg, ahbg->ahbc", [timevec2, adp3])
+        adp = F.softmax(F.relu(adp4), dim=3)
+        return adp
+
+class temporal_block(nn.Module):
+    def __init__(self, drop, num_nodes, num_steps, N_t, device, temporal_dim, hid_dim):
+        super(temporal_block, self).__init__()
+        self.drop = drop
+        self.temporal_graph_generator = temporal_graph_generator(num_nodes, num_steps, device, N_t, temporal_dim)
+        self.tcn = Tcn(hid_dim, hid_dim)
+
+    def forward(self, x, t):
+        x = self.tcn(x)
+        adp = self.temporal_graph_generator(t)
+        out = torch.einsum("ncvl, nvlw->ncvw", [x, adp])
+        out = F.dropout(out, self.drop, training=self.training)
+        return out

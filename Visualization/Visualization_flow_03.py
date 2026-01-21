0c4eb2b2db8500440064e8f1b1934a9e233dc547
Pan Tianyi <98876418+Typan2000@users.noreply.github.com>
2026-01-21 23:09:38 +0800

    Add files via upload

---
+++ Visualization/Visualization_flow_03.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+node = 76 #0-357
+data = np.load('data/Predictive Traffic Visualization03.npy')
+x = np.linspace(4, 20, 192, endpoint=False)
+ground_truth = data[0, node]
+prediction = data[1, node]
+plt.figure(figsize=(9, 6))
+plt.xlabel('Time',fontsize=28)
+plt.ylabel('Traffic Flow',fontsize=28)
+plt.tick_params(axis='y', labelsize=20)
+plt.xticks(np.arange(4, 21, 4), [f'{i}:00' for i in range(4, 21, 4)], fontsize=20)
+plt.plot(x, ground_truth, markersize=6, linestyle='-', label='Gound Truth')
+plt.plot(x, prediction, markersize=4, linestyle='-', label='Prediction', color='red')
+plt.legend(loc='upper right',fontsize=13)
+plt.show()
